c0716690ddce41e603dda664a9f8ddbd5836b2cf
ZxZxZ154 <kyrlukov205@gmail.com>
2026-03-09 22:50:41 +0500

    feat: initial project structure

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+tasks = []
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def menu():
+    while True:
+        print("\nTask Manager")
+        print("1 Add task")
+        print("2 List tasks")
+        print("0 Exit")
+
+        choice = input("Choose: ")
+
+        if choice == "1":
+            print("Add task not implemented yet")
+        elif choice == "2":
+            print("List tasks not implemented yet")
+        elif choice == "0":
+            break
+        else:
+            print("Invalid choice")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+menu()
