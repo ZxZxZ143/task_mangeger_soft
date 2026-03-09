3bc6c32e267f20bc9c7b935f9221518eceac3a52
ZxZxZ154 <kyrlukov205@gmail.com>
2026-03-09 22:51:20 +0500

    feat: add task creation feature

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 tasks = []
 
 
+class Task:
+    def __init__(self, task_id, title, description, status="Pending"):
+        self.id = task_id
+        self.title = title
+        self.description = description
+        self.status = status
+
+
+def generate_id():
+    if len(tasks) == 0:
+        return 1
+    return tasks[-1].id + 1
+
+
+def add_task():
+    title = input("Enter title: ")
+    description = input("Enter description: ")
+
+    task = Task(generate_id(), title, description, "Pending")
+    tasks.append(task)
+
+    print("Task added")
+
+
 def menu():
     while True:
         print("\nTask Manager")
@@ -11,7 +35,7 @@ def menu():
         choice = input("Choose: ")
 
         if choice == "1":
-            print("Add task not implemented yet")
+            add_task()
         elif choice == "2":
             print("List tasks not implemented yet")
         elif choice == "0":
